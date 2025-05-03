bccbbac9e0b9983dc845b50e5f465a5d58071f85
Eztas <nietzsche2669sensei@gmail.com>
2025-05-03 12:39:48 +0000

    update; c問題, テストケース1つクリア, Noの対応だけ追加

--- contest/abc404/c/main.py
+++ contest/abc404/c/main.py
@@ -3,4 +3,13 @@ import sys
 data = sys.stdin.read().splitlines()
 N, M = map(int, data[0].split())
 
-print(N, M)
+graph = [0 for _ in range(N)]
+
+for i in range(1, M + 1):
+    side = list(map(int, data[i].split(' ')))
+    if graph[side[0] - 1] == 0 or graph[side[0] - 1] == side[1]:
+        graph[side[0] - 1] = side[1]
+    else:
+        print("No")
+        exit()
+        
