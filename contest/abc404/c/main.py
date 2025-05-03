0ce135575286332e0ecb409ccf7f1ba185857b9e
Eztas <nietzsche2669sensei@gmail.com>
2025-05-03 12:30:24 +0000

    add: c問題, 1行目のN, M確認

--- contest/abc404/c/main.py
+++ contest/abc404/c/main.py
@@ -1,7 +1,6 @@
 import sys
-n = int(input())
-n,m = map(int,input().split())
 
-a = list(map(int,input().split()))
-li = [list(map(int, input().split())) for _ in range(n)]
 data = sys.stdin.read().splitlines()
+N, M = map(int, data[0].split())
+
+print(N, M)
