324c39b3535a15f4e8157269cd717be6107b4dbf
Eztas <nietzsche2669sensei@gmail.com>
2025-05-03 12:11:32 +0000

    add: b問題, 1行目のN確認

--- contest/abc404/b/main.py
+++ contest/abc404/b/main.py
@@ -1,7 +1,7 @@
 import sys
-n = int(input())
-n,m = map(int,input().split())
 
-a = list(map(int,input().split()))
-li = [list(map(int, input().split())) for _ in range(n)]
 data = sys.stdin.read().splitlines()
+
+N = int(data[0])
+
+print(N)
