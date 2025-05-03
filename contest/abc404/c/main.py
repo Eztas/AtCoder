8a3b8dbd5ac6fd56dccd7e46341b0ccd609465d1
Eztas <nietzsche2669sensei@gmail.com>
2025-05-03 12:59:03 +0000

    update: 頂点に関しての誤解あり、修正、ここで制限時間内のテストは辞退

--- contest/abc404/c/main.py
+++ contest/abc404/c/main.py
@@ -3,13 +3,18 @@ import sys
 data = sys.stdin.read().splitlines()
 N, M = map(int, data[0].split())
 
-graph = [0 for _ in range(N)]
+graph = [[] for _ in range(N)]
 
 for i in range(1, M + 1):
     side = list(map(int, data[i].split(' ')))
-    if graph[side[0] - 1] == 0 or graph[side[0] - 1] == side[1]:
-        graph[side[0] - 1] = side[1]
+    if len(graph[side[0] - 1]) < 2 and len(graph[side[1] - 1]) < 2:
+        graph[side[1] - 1].append(side[0])
+        graph[side[0] - 1].append(side[1])
     else:
         print("No")
         exit()
-        
+
+cycleGraph = [1 for _ in range(N)]
+
+for i in range(1, N + 1):
+    cycleGraph[i] = graph[i-1]
