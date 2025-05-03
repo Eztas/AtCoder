db2a12fdd5c61f433dc876c0f8d65bd8a55d1189
Eztas <nietzsche2669sensei@gmail.com>
2025-05-03 12:09:29 +0000

    solve: a問題クリア(テストケース上では2つのみクリアだが、実際は全クリ)

--- contest/abc404/a/main.py
+++ contest/abc404/a/main.py
@@ -1,7 +1,17 @@
 import sys
-n = int(input())
-n,m = map(int,input().split())
 
-a = list(map(int,input().split()))
-li = [list(map(int, input().split())) for _ in range(n)]
 data = sys.stdin.read().splitlines()
+
+alphabetList = ['a', 'b', 'c', 'd', 'e',
+                'f', 'g', 'h', 'i', 'j',
+                'k', 'l', 'm', 'n', 'o',
+                'p', 'q', 'r', 's', 't',
+                'u', 'v', 'w', 'x', 'y', 'z']
+
+for alphabetForDelete in data[0]:
+    for alphabet in alphabetList:
+        if alphabetForDelete == alphabet:
+            alphabetList.remove(alphabet)
+            break
+
+print(alphabetList[0])
