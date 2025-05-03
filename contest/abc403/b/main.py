5249dad9ffcedc3320d6ccb60523a9335fa3edee
Eztas <nietzsche2669sensei@gmail.com>
2025-05-03 09:04:08 +0000

    update; abc403, b問題テストケース3つクリア

--- contest/abc403/b/main.py
+++ contest/abc403/b/main.py
@@ -6,6 +6,7 @@ flag = False
 for i in range(len(data[0]) - len(data[1]) + 1):
     for idx, word in enumerate(data[1]):
         if data[0][i + idx] != word and data[0][i + idx] != '?':
+            flag = False
             break
 
         flag = True
