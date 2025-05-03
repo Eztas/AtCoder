6972847eb9baf8372699c0fa42fd92d8f5120038
Eztas <nietzsche2669sensei@gmail.com>
2025-05-03 12:14:41 +0000

    update: b問題, S, Tの内容追加、しかし苦手問題なので一度断念

--- contest/abc404/b/main.py
+++ contest/abc404/b/main.py
@@ -4,4 +4,11 @@ data = sys.stdin.read().splitlines()
 
 N = int(data[0])
 
-print(N)
+S = []
+T = []
+
+for i in range(1, N + 1):
+    S.append(data[i].split(' '))
+
+for i in range(N + 1, 2 * N + 1):
+    T.append(data[i].split(' '))
