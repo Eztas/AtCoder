d9112c3544aa3b721136476a5a7249c9193c2c51
Eztas <nietzsche2669sensei@gmail.com>
2025-05-10 13:00:41 +0000

    add: d問題, 入力形式だけ追加

--- contest/abc405/d/main.py
+++ contest/abc405/d/main.py
@@ -1,7 +1,2 @@
-import sys
-n = int(input())
-n,m = map(int,input().split())
-
-a = list(map(int,input().split()))
-li = [list(map(int, input().split())) for _ in range(n)]
-data = sys.stdin.read().splitlines()
+H, W = map(int,input().split())
+S = [list(map(int, input().split())) for _ in range(n)]
