ba10dcd23bcb80f2ab7fe2bfd1fdedd327f53ec6
Eztas <nietzsche2669sensei@gmail.com>
2025-05-10 15:44:45 +0000

    add: abc405, g問題、剰余演算を忘れていたので、実行するも結局クリア数は一切変わらず

--- contest/abc405/g/main.py
+++ contest/abc405/g/main.py
@@ -7,17 +7,13 @@ A = list(map(int,input().split()))
 for i in range(Q):
     L, R, X = map(int,input().split())
     B = A[L-1:R]
-
-    for j in range(len(B)):
-        if B[j] >= X:
-            B[j] = 0
     
-    newB = [b for b in B if b != 0]
+    newB = [b for b in B if b < X]
     newBCounts = collections.Counter(newB).values()
     combination = math.factorial(sum(newBCounts))
 
     for count in newBCounts:
         combination /= math.factorial(count)
 
-    print(int(combination))
+    print(int(combination % 998244353))
     
