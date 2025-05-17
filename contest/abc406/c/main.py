e76b2da68d172d7bf82e91146cfebe87969e2fcb
Eztas <nietzsche2669sensei@gmail.com>
2025-05-17 13:07:37 +0000

    add: abc406, c問題, 3, 4つ目の条件追加するもテストケース1つのみクリア

--- contest/abc406/c/main.py
+++ contest/abc406/c/main.py
@@ -10,3 +10,26 @@ for n in range(4, N):
         if P[i] >= P[i+1]:
             flag = False
             break
+
+        # A_i−1 ​< A_i > A_i+1 ​を満たす 2 ≤ i <|A| なる整数 i はちょうど 1 個である
+        a_count1 = 0
+        for j in range(i+1, i+n-1):
+            if P[j] > P[j-1] and P[j] > P[j+1]:
+                a_count1 += 1
+        if a_count1 != 1:
+            flag = False
+            break
+
+        # A_i−1 ​> A_i < A_i+1 ​を満たす 2 ≤ i <|A| なる整数 i はちょうど 1 個である
+        a_count2 = 0
+        for j in range(i+1, i+n-1):
+            if P[j] < P[j-1] and P[j] < P[j+1]:
+                a_count2 += 1
+        if a_count2 != 1:
+            flag = False
+            break
+
+        if flag:
+            count += 1
+
+print(count)
