970a1ed667c8a9da0a2f4e4a8f9dcd01b9a67520
Eztas <nietzsche2669sensei@gmail.com>
2025-05-17 13:22:20 +0000

    add: abc406, c問題, サンプル全てクリアもTLE

--- contest/abc406/c/main.py
+++ contest/abc406/c/main.py
@@ -6,7 +6,7 @@ count = 0
 for n in range(4, N+1):
     for i in range(N-n+1):
         A = P[i:i+n]
-        
+
         # A_1 < A_2である 
         if A[0] >= A[1]:
             continue
@@ -15,7 +15,7 @@ for n in range(4, N+1):
         a_count1 = 0
         # A_i−1 ​> A_i < A_i+1 ​を満たす 2 ≤ i <|A| なる整数 i はちょうど 1 個である
         a_count2 = 0
-        for j in range(2, n-1):
+        for j in range(1, n-1):
             if A[j] > A[j-1] and A[j] > A[j+1]:
                 a_count1 += 1
             if A[j] < A[j-1] and A[j] < A[j+1]:
