a98b2cec8babb2761ac5b6b70db1f4b790918cb5
Eztas <nietzsche2669sensei@gmail.com>
2025-05-17 12:35:47 +0000

    add: abc406, d問題, クエリ1, 2のごみのカウント機能のみ追加

--- contest/abc406/d/main.py
+++ contest/abc406/d/main.py
@@ -7,3 +7,19 @@ print(blocks)
 for n in range(N):
     x, y = map(int, input().split())
     blocks[x-1][y-1] = 0
+
+Q = int(input())
+for _ in range(Q):
+    q, xy = map(int, input().split())
+    count = 0
+    if q == 1:
+        for block in blocks[xy-1]:
+            if block == 0:
+                count += 1
+        print(count)
+    elif q == 2:
+        for h in range(H):
+            if blocks[h][xy-1] == 0:
+                count += 1
+        print(count)
+        
