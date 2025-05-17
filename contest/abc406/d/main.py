d07b0867b5cd3acd941cda364d87f485eda81f91
Eztas <nietzsche2669sensei@gmail.com>
2025-05-17 12:43:29 +0000

    add: abc406, d問題, サンプルはすべてクリアするもREが生じる

--- contest/abc406/d/main.py
+++ contest/abc406/d/main.py
@@ -2,24 +2,31 @@ H, W, N = map(int,input().split())
 
 blocks = [[1 for _ in range(W)] for _ in range(H)]
 
-print(blocks)
-
 for n in range(N):
     x, y = map(int, input().split())
     blocks[x-1][y-1] = 0
 
 Q = int(input())
+trash_num = N
 for _ in range(Q):
     q, xy = map(int, input().split())
     count = 0
+
+    if trash_num == 0:
+        print(0)
+        continue
+
     if q == 1:
-        for block in blocks[xy-1]:
-            if block == 0:
+        for w in range(W):
+            if blocks[xy-1][w] == 0:
                 count += 1
-        print(count)
+                blocks[xy-1][w] = 1
+
     elif q == 2:
         for h in range(H):
             if blocks[h][xy-1] == 0:
                 count += 1
-        print(count)
-        
+                blocks[h][xy-1] = 1
+    
+    trash_num -= count
+    print(count)
