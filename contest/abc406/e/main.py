20629c83a974994a4b90c1f2b3602b6825427edb
Eztas <nietzsche2669sensei@gmail.com>
2025-05-24 09:58:55 +0900

    add: abc406, e問題, 実行も遅く失敗するも一度追加

--- contest/abc406/e/main.py
+++ contest/abc406/e/main.py
@@ -1,7 +1,17 @@
 import sys
-n = int(input())
-n,m = map(int,input().split())
+T = int(input())
 
-a = list(map(int,input().split()))
-li = [list(map(int, input().split())) for _ in range(n)]
-data = sys.stdin.read().splitlines()
+for _ in range(T):
+    N, K = map(int,input().split())
+    total = 0
+
+    for x in range(1, N+1):
+        bin_x = bin(x)[2:]
+        count = 0
+        for bin_x_bit in bin_x:
+            if bin_x_bit == 1:
+                count += 1
+        if count == K:
+            total += x
+
+    print(total % 998244353)
