ae9728bd64d9fd7eaaddffac9849e043ef75279a
Eztas <nietzsche2669sensei@gmail.com>
2025-05-30 14:05:16 +0000

    add: abc407, c問題, テストケース12つクリア, 残り5つTLE

--- contest/abc407/c/main.py
+++ contest/abc407/c/main.py
@@ -1,10 +1,18 @@
 S = input()
-S_len = len(S)
-count = S_len # Aの回数は必ずS_len回
+count = len(S) # Aの回数はSの長さと同じなので、最初から追加
 
-B_num = 0
-for idx in range(S_len):
-    B_num += int(S[S_len - idx - 1])
-    count += B_num % 10
+while True:
+    last_S = int(S[len(S)-1])
+    count += last_S # Bの回数を加算
+    next_S = []
+    for digit in S:
+        if int(digit) < last_S:
+            next_S.append(str(10 + int(digit) - last_S))      
+        else:
+            next_S.append(str(int(digit) - last_S))
+
+    S = next_S[:-1]
+    if len(S) == 0:
+        break
 
 print(count)
