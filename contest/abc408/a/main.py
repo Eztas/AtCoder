46c6e2724d85994a1fadf6e8104f8f5701511572
Eztas <nietzsche2669sensei@gmail.com>
2025-05-31 21:25:44 +0900

    add: abc408, a問題, 全テストケースクリア, Tの先頭に0を追加し、0からの時間差も検討できるようにした

--- contest/abc408/a/main.py
+++ contest/abc408/a/main.py
@@ -1,10 +1,10 @@
 N, S = map(int,input().split())
-t = list(map(int,input().split()))
+T = list(map(int,input().split()))
 
 sleep = "Yes"
-T = [0, t]
+T.insert(0, 0)  # 最初の叩く時間を0に設定
 
-for n in range(N-1):
+for n in range(N):
   if T[n+1] - T[n] >= S + 0.5: # 次に叩くまでS+0.5未満で叩けたら老人は寝ない
     sleep = "No"
     break
