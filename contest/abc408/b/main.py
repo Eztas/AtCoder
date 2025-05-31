73fec636c1f6f2f740dcdd00a78fb3118c3f6aff
Eztas <nietzsche2669sensei@gmail.com>
2025-05-31 21:38:33 +0900

    bug: abc408, b問題, なぜか要件を満たす答えであるが、全てWA

--- contest/abc408/b/main.py
+++ contest/abc408/b/main.py
@@ -2,5 +2,7 @@ N = int(input())
 A = list(map(int,input().split()))
 A_sorted = sorted(list(set(A)))
 
-for a in A_sorted:
+for idx, a in enumerate(A_sorted):
     print(a,end='')
+    if idx < len(A_sorted) - 1:
+        print(' ',end='')
