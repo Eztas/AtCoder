5b02845f93846183732c72f82f714383f4659337
Eztas <nietzsche2669sensei@gmail.com>
2025-05-31 21:17:51 +0900

    add: abc408, a問題, テストケース24/27クリア

--- contest/abc408/a/main.py
+++ contest/abc408/a/main.py
@@ -1,7 +1,11 @@
-import sys
-n = int(input())
-n,m = map(int,input().split())
+N, S = map(int,input().split())
+T = list(map(int,input().split()))
 
-a = list(map(int,input().split()))
-li = [list(map(int, input().split())) for _ in range(n)]
-data = sys.stdin.read().splitlines()
+sleep = "Yes"
+
+for n in range(N-1):
+  if T[n+1] - T[n] >= S + 0.5: # 次に叩くまでS+0.5未満で叩けたら老人は寝ない
+    sleep = "No"
+    break
+  
+print(sleep)
