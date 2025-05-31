de733eb7724932d86de7bf7176e2baa17e41c5ba
Eztas <nietzsche2669sensei@gmail.com>
2025-05-31 21:20:59 +0900

    add: abc408, a問題, 0秒からT_1までの間も含めるもREになる

--- contest/abc408/a/main.py
+++ contest/abc408/a/main.py
@@ -1,7 +1,8 @@
 N, S = map(int,input().split())
-T = list(map(int,input().split()))
+t = list(map(int,input().split()))
 
 sleep = "Yes"
+T = [0, t]
 
 for n in range(N-1):
   if T[n+1] - T[n] >= S + 0.5: # 次に叩くまでS+0.5未満で叩けたら老人は寝ない
