552be0036a06291e13d88ef8738bd48987387933
Eztas <nietzsche2669sensei@gmail.com>
2025-06-07 21:27:04 +0900

    bug: abc409, b問題. なぜか2ループ目で強制終了する, ０か1しか出ない

--- contest/abc409/b/main.py
+++ contest/abc409/b/main.py
@@ -1,7 +1,23 @@
-import sys
-n = int(input())
-n,m = map(int,input().split())
+N = int(input())
+A = map(int,input().split())
 
-a = list(map(int,input().split()))
-li = [list(map(int, input().split())) for _ in range(n)]
-data = sys.stdin.read().splitlines()
+x = 0
+
+while True:
+    flag = True
+    count = 0
+    for a in A:
+        print('a-', a, 'x=',x)
+        if a >= x:
+            count += 1
+    print(count, x)
+    if count < x:
+        flag = False
+    
+    if not flag:
+        if x != 0:
+          x -= 1
+        break
+    x += 1
+
+print(x)
