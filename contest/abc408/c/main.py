96ce0792faad7d41af561d07576198490cb75f47
Eztas <nietzsche2669sensei@gmail.com>
2025-06-07 22:21:49 +0900

    add: abc409, c問題, 距離行列の考え方のみ追加

--- contest/abc408/c/main.py
+++ contest/abc408/c/main.py
@@ -1,7 +1,17 @@
-import sys
-n = int(input())
-n,m = map(int,input().split())
+N, L = int(input())
+D = list(map(int,input().split()))
 
-a = list(map(int,input().split()))
-li = [list(map(int, input().split())) for _ in range(n)]
-data = sys.stdin.read().splitlines()
+# 円周における距離行列を作る
+
+dist = [[0] * N for _ in range(N)]
+
+# 1
+# 1-2 = d1 = d1 % 6,  min(L-d1, d1) 1-3 = d2 = d1 + d2 % 6,  min(L-d2, d2) 
+# 1-4 = d3 = d2 + d3 % 6,  min(L-d3, d3), 1-5 = d4 = d3 + d4 % 6,  min(L-d4, d4)
+
+for i in range(N):
+    for j in range(N):
+        if i == j:
+            dist[i][j] = 0
+        else:
+            dist[i][j] = abs(D[i] - D[j])
