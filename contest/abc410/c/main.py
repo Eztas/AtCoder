3a0aea1a4c777cf3197337c3d6ab2d85ca651cbc
Eztas <nietzsche2669sensei@gmail.com>
2025-06-14 21:40:52 +0900

    add: abc410, c, Aの変化をプリントデバッグ

--- contest/abc410/c/main.py
+++ contest/abc410/c/main.py
@@ -1,7 +1,21 @@
-import sys
-n = int(input())
-n,m = map(int,input().split())
+N, Q = map(int,input().split())
 
-a = list(map(int,input().split()))
-li = [list(map(int, input().split())) for _ in range(n)]
-data = sys.stdin.read().splitlines()
+A = [n+1 for n in range(N)]
+
+for q in range(Q):
+    query = list(map(int,input().split()))
+
+    if query[0] == 1:
+        p = query[1] - 1
+        x  = query[2]
+        A[p] = x
+
+    elif query[0] == 2:
+        p = query[1] - 1
+        print(A[p])
+    
+    else:  # query[0] == 3
+        k = query[1]
+        A = A[-k:] + A[0:k] # 先頭を末尾に移動をk回繰り返す = ０からk-1番目の要素群を末尾に移動
+
+    print(A)
