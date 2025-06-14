c662795558d488cd42ba8c0ee4c3b8f7b33fc8ea
Eztas <nietzsche2669sensei@gmail.com>
2025-06-14 13:34:53 +0000

    test: abc410, c, キューを試す, 実行自体は問題なし

--- contest/abc410/c/main.py
+++ contest/abc410/c/main.py
@@ -1,3 +1,4 @@
+from collections import deque
 N, Q = map(int,input().split())
 
 A = [n+1 for n in range(N)]
@@ -16,9 +17,12 @@ for q in range(Q):
     
     else:  # query[0] == 3
         k = query[1] % N # kがN以上の場合はNで割った余りを使う, 実質余りの数しか入れ替えが起きない(k=Nなら元に戻るから)
-        if k == 0: # k=0なら移動はないのでスルー
-            continue
-        A = A[k:] + A[0:k] # 先頭を末尾に移動をk回繰り返す = 0からk-1番目の要素群を末尾に移動
+        if k != 0: # k=0なら移動はないのでスルー
+            A_deque = deque(A)
+            for i in range(k):
+                a = A_deque.popleft()
+                A_deque.append(a) 
+            A = list(A_deque)
 
 if not type_2_exist:
     print()
