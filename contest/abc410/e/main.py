02ddefc91fc905efdd29a7e14d15c176230bae82
Eztas <nietzsche2669sensei@gmail.com>
2025-06-14 21:56:27 +0900

    add: abc410, e, 単純な2パターンをいったん追加と今後の考え追加

--- contest/abc410/e/main.py
+++ contest/abc410/e/main.py
@@ -1,7 +1,11 @@
-import sys
-n = int(input())
-n,m = map(int,input().split())
-
-a = list(map(int,input().split()))
-li = [list(map(int, input().split())) for _ in range(n)]
-data = sys.stdin.read().splitlines()
+N, H, M = map(int,input().split())
+# 実際はforループで順番に倒すとかではなく
+# 総合計を計算して出力するはず
+# 体力を1,3番目に, 魔法を2, 4, 5番目に使うと最も敵を倒せる、みたいに
+for n in range(N):
+    A, B = map(int,input().split())
+    
+    if A <= H and A < B:
+        H = H - A
+    elif B <= W and A >= B:
+        W = W - B
