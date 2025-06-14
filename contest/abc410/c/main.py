cea1c1a03283a954d59d1083c7aaf5f0039ccd17
Eztas <nietzsche2669sensei@gmail.com>
2025-06-14 13:10:58 +0000

    add: abc410, c, TLE20だがどうしようもない、O(N+Q)以上速くする手立てがない(kの入れ替えも計算量はいるか？O(N+Q+k)か？)

--- contest/abc410/c/main.py
+++ contest/abc410/c/main.py
@@ -9,7 +9,7 @@ for q in range(Q):
 
     if query[0] == 1:
         p = query[1] - 1
-        x  = query[2]
+        x = query[2]
         A[p] = x
 
     elif query[0] == 2:
@@ -18,9 +18,10 @@ for q in range(Q):
         print(A[p])
     
     else:  # query[0] == 3
-        k = query[1]
+        k = query[1] % N # kがN以上の場合はNで割った余りを使う, 実質余りの数しか入れ替えが起きない(k=Nなら元に戻るから)
+        if k == 0: # k=0なら移動はないのでスルー
+            continue
         A = A[k:] + A[0:k] # 先頭を末尾に移動をk回繰り返す = ０からk-1番目の要素群を末尾に移動
 
 if not type_2_exist:
     print()
-    
