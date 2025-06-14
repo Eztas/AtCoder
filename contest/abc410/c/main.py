c258c57f644875a9ad2c2e73f751c5061d10db60
Eztas <nietzsche2669sensei@gmail.com>
2025-06-14 13:16:03 +0000

    add: abc410, c, TLEが2個増えてたのでまず細かい修正だけ行う

--- contest/abc410/c/main.py
+++ contest/abc410/c/main.py
@@ -8,20 +8,17 @@ for q in range(Q):
     query = list(map(int,input().split()))
 
     if query[0] == 1:
-        p = query[1] - 1
-        x = query[2]
-        A[p] = x
+        A[query[1] - 1] = query[2]
 
     elif query[0] == 2:
         type_2_exist = True
-        p = query[1] - 1
-        print(A[p])
+        print(A[query[1] - 1])
     
     else:  # query[0] == 3
         k = query[1] % N # kがN以上の場合はNで割った余りを使う, 実質余りの数しか入れ替えが起きない(k=Nなら元に戻るから)
         if k == 0: # k=0なら移動はないのでスルー
             continue
-        A = A[k:] + A[0:k] # 先頭を末尾に移動をk回繰り返す = ０からk-1番目の要素群を末尾に移動
+        A = A[k:] + A[0:k] # 先頭を末尾に移動をk回繰り返す = 0からk-1番目の要素群を末尾に移動
 
 if not type_2_exist:
     print()
