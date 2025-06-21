f55d2639bb71dce141b0c14b89eb4c670cc23c72
Eztas <nietzsche2669sensei@gmail.com>
2025-06-21 21:50:51 +0900

    think: abc411, c, indexで管理する方法を考えるも昇華できず

--- contest/abc411/c/main.py
+++ contest/abc411/c/main.py
@@ -3,15 +3,10 @@ A = list(map(int,input().split()))
 
 # 白を0, 黒を1とする, 左右1列
 line = [0] * N
-
+black_list = []
 for q in range(Q):
     count = 0
     line[A[q] - 1] = 1 - line[A[q] - 1]  # A[q]の色を反転
-    black_time = False
-    for n in range(N):
-        if line[n] == 1 and not black_time:
-            black_time = True
-            count += 1
-        elif line[n] == 0 and black_time:
-            black_time = False
+    black_list.append(A[q] - 1)  #  黒の位置を記録
+    count += 1
     print(count)
