a470041b242915d09d656916197451db2e4ac63e
Eztas <nietzsche2669sensei@gmail.com>
2025-06-21 21:14:41 +0900

    add: abc411, b, 計算が合わない

--- contest/abc411/b/main.py
+++ contest/abc411/b/main.py
@@ -1,7 +1,11 @@
-import sys
-n = int(input())
-n,m = map(int,input().split())
+N = int(input())
+D = list(map(int,input().split()))
 
-a = list(map(int,input().split()))
-li = [list(map(int, input().split())) for _ in range(n)]
-data = sys.stdin.read().splitlines()
+for i in range(N-1):
+    count = 0
+    for j in range(i+1, N):
+        count += (D[j] - D[i])
+        if j == N-2:
+            print(count)
+        else:
+            print(count,end=' ')
