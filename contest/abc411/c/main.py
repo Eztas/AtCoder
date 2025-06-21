bff537125011bd8b976dd775a452e5f99ea3acc0
Eztas <nietzsche2669sensei@gmail.com>
2025-06-21 21:42:13 +0900

    tle: abc411, c, 6/29しかクリア出来ず

--- contest/abc411/c/main.py
+++ contest/abc411/c/main.py
@@ -7,5 +7,11 @@ line = [0] * N
 for q in range(Q):
     count = 0
     line[A[q] - 1] = 1 - line[A[q] - 1]  # A[q]の色を反転
+    black_time = False
+    for n in range(N):
+        if line[n] == 1 and not black_time:
+            black_time = True
+            count += 1
+        elif line[n] == 0 and black_time:
+            black_time = False
     print(count)
-    
