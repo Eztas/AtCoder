3760a98843a752dc8240d117c3889aa684c4b370
Eztas <nietzsche2669sensei@gmail.com>
2025-06-21 21:18:04 +0900

    solve: abc411, b, Dの解釈を勘違いするも解決

--- contest/abc411/b/main.py
+++ contest/abc411/b/main.py
@@ -3,8 +3,8 @@ D = list(map(int,input().split()))
 
 for i in range(N-1):
     count = 0
-    for j in range(i+1, N):
-        count += (D[j] - D[i])
+    for j in range(i, N-1):
+        count += D[j]
         if j == N-2:
             print(count)
         else:
