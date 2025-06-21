cf5a3a620dff92964f83bfd19194a6167a7b9999
Eztas <nietzsche2669sensei@gmail.com>
2025-06-21 13:20:14 +0000

    tle: abc411, d, これは本当に時短するところが思いつかない, 33/49クリア

--- contest/abc411/d/main.py
+++ contest/abc411/d/main.py
@@ -1,7 +1,17 @@
-import sys
-n = int(input())
-n,m = map(int,input().split())
+N, Q = map(int,input().split())
 
-a = list(map(int,input().split()))
-li = [list(map(int, input().split())) for _ in range(n)]
-data = sys.stdin.read().splitlines()
+server = ""
+pc = ["" for _ in range(N)]
+
+for q in range(Q):
+    query = input().split()
+    if query[0] == "1":
+        pc[int(query[1])-1] = server
+
+    elif query[0] == "2":
+        pc[int(query[1]) - 1] += query[2]
+        
+    elif query[0] == "3":
+        server = pc[int(query[1])-1]
+
+print(server)
