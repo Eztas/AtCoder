bd37cbf3bf45f2cf8af7a164bb0d35e98056d368
Eztas <nietzsche2669sensei@gmail.com>
2025-06-28 12:43:12 +0000

    think: abc412, c,考え方の追加

--- contest/abc412/c/main.py
+++ contest/abc412/c/main.py
@@ -1,5 +1,8 @@
 T = int(input())
 
+# ドミノ i のすぐ右に置かれているドミノの大きさが 2S_i以下ならばそのドミノも右に向けて倒れます。
+# 2倍する->探索->を繰り返す, T*N*N->10^15
+
 for t in range(T):
     N = int(input())
     S = list(map(int,input().split()))
