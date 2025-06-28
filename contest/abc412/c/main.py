50bb51d43af70919bbe4c8b001b81b95910fc4b8
Eztas <nietzsche2669sensei@gmail.com>
2025-06-28 13:10:14 +0000

    add: abc412, c, テストケースクリア, 探索を行う計算を追加, おそらくN*T>10^10なのでおそらく無理

--- contest/abc412/c/main.py
+++ contest/abc412/c/main.py
@@ -9,4 +9,19 @@ for t in range(T):
     N = int(input())
     S = list(map(int,input().split()))
     count = math.log2(S[N-1]//S[0])
-    print(math.ceil(count) + 1)  # 0-indexedなので+1
+    domino = S[0]
+    for c in range(math.ceil(count)):
+        domino_flag = False
+        for n in range(N):
+            if domino < S[n] and S[n] <= 2 * domino:
+                domino = S[n]
+                domino_flag = True
+                break
+
+        if not domino_flag:
+            print(-1)
+            break
+
+    if domino_flag:
+        print(math.ceil(count) + 1)
+    
