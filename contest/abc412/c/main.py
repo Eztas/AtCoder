d1dfa2c6dc89c09df60f733f4c445d4e7bb57bab
Eztas <nietzsche2669sensei@gmail.com>
2025-06-28 12:54:18 +0000

    add: abc412, c, もし実行可能なときの最小回数を追加

--- contest/abc412/c/main.py
+++ contest/abc412/c/main.py
@@ -1,8 +1,12 @@
+import math
 T = int(input())
 
 # ドミノ i のすぐ右に置かれているドミノの大きさが 2S_i以下ならばそのドミノも右に向けて倒れます。
 # 2倍する->探索->を繰り返す, T*N*N->10^15
+# log_2(N)≒16sで済ませる方法があるはず
 
 for t in range(T):
     N = int(input())
     S = list(map(int,input().split()))
+    count = math.log2(S[N-1]//S[0])
+    print(math.ceil(count) + 1)  # 0-indexedなので+1
