a0507ae01674abc90d8ec75e9d35d92ef7be1897
Eztas <nietzsche2669sensei@gmail.com>
2025-07-05 13:26:24 +0000

    add: abc413, c, TLE, AC13, TLE10, (2*(10^5))^2だから少し超過した

--- contest/abc413/c/main.py
+++ contest/abc413/c/main.py
@@ -1,6 +1,7 @@
 Q = int(input())
 
-A = []
+C = []
+X = []
 
 # 1 c x, xをc個追加, 下手すればO(10^9)なので正攻法はダメ
 # このとき、k はその時点での A の長さ以下であることが保証される。
@@ -12,8 +13,19 @@ for _ in range(Q):
     if query[0] == 1:
         c = query[1]
         x = query[2]
-        A.extend([x] * c)
+        C.append(c)
+        X.append(x)
     elif query[0] == 2:
         k = query[1]
-        print(sum(A[:k]))
-        A = A[k:]
+        sumSlice = 0
+        for idx, c in enumerate(C):
+            if c >= k:
+                sumSlice += X[idx] * k
+                C[C.index(c)] -= k
+                break
+            else:
+                sumSlice += X[idx] * c
+                k -= c
+                C[C.index(c)] = 0
+
+        print(sumSlice)
