b39caf149e1208b91e39ea6ff369c57ce932dbf5
Eztas <nietzsche2669sensei@gmail.com>
2025-07-05 12:03:45 +0000

    solve: abc413, a

--- contest/abc413/a/main.py
+++ contest/abc413/a/main.py
@@ -1,4 +1,8 @@
-S = input()
-L = int(input())
-D = list(map(int,input().split()))
-N, Q = map(int,input().split())
+N, M = map(int,input().split())
+A = list(map(int,input().split()))
+
+if M >= sum(A):
+    print('Yes')
+else:
+    print('No')
+    
