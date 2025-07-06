5d179a0105771f35073ecc09976aed206007a5b2
Eztas <nietzsche2669sensei@gmail.com>
2025-07-06 00:12:53 +0000

    update: abc413, b, set()を使うとかなり楽に終わる

--- contest/abc413/b/main.py
+++ contest/abc413/b/main.py
@@ -5,21 +5,11 @@ for n in range(N):
     s = input()
     S.append(s)
 
-count = 0
-combinedwords = []
+combinations = set()
 
 for i in range(N):
     for j in range(N):
-        existed = False
-        if i == j:
-            continue
-        else:
-            for combinedword in combinedwords:
-                if S[i] + S[j] == combinedword:
-                    existed = True
-                    continue
-            if not existed:
-                combinedwords.append(S[i] + S[j])
-                count += 1
-
-print(count)
+        if i != j:
+            combinations.add(S[i] + S[j])
+            
+print(len(combinations))
