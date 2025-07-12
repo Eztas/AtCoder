feb5a25aea770f953e117c9db7d1aacf9492d2f8
Eztas <nietzsche2669sensei@gmail.com>
2025-07-12 12:16:49 +0000

    solve: abc414, b, 文字列のmapに思ったより苦戦

--- contest/abc414/b/main.py
+++ contest/abc414/b/main.py
@@ -1,4 +1,20 @@
-S = input()
-L = int(input())
-D = list(map(int,input().split()))
-N, Q = map(int,input().split())
+N = int(input())
+count = 0
+S = ""
+isTooLong = False
+for n in range(N):
+    c, l = map(str, input().split()) # strをつけないとmapできない
+
+    count += int(l)
+    if count > 100:
+        isTooLong = True
+        break
+
+    for i in range(int(l)):
+        S += c # appendではなく、+=で文字列を連結する
+
+if isTooLong:
+    print("Too Long")
+
+else:
+    print(S)
