55bf15e2e1fe129443e79b648ae4ae827fbff0f3
Eztas <nietzsche2669sensei@gmail.com>
2025-07-12 12:42:18 +0000

    add: abc414, c, 回文判定, 進法変換機能追加, 計算量超過でサンプル1つだけクリア

--- contest/abc414/c/main.py
+++ contest/abc414/c/main.py
@@ -1,4 +1,16 @@
-S = input()
-L = int(input())
-D = list(map(int,input().split()))
-N, Q = map(int,input().split())
+A = int(input())
+N = int(input())
+
+def base10int(value, base):
+    if (int(value / base)):
+        return base10int(int(value / base), base) + str(value % base)
+    return str(value % base)
+
+total = 0
+for n in range(N):
+    n_str = str(n)
+    n_a = base10int(n, A)
+    if n_str == n_str[::-1] and n_a == n_a[::-1]:
+        total += n
+
+print(total)
