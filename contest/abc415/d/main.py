a246071664998f56d81443b7d4f6f3e6bfd8499d
Eztas <nietzsche2669sensei@gmail.com>
2025-07-19 12:54:05 +0000

    wip: abc415, d, 考え方追加

--- contest/abc415/d/main.py
+++ contest/abc415/d/main.py
@@ -1,4 +1,27 @@
-S = input()
-L = int(input())
-D = list(map(int,input().split()))
-N, Q = map(int,input().split())
+N, M = map(int,input().split())
+
+# 初期値=N本の空いていないコーラ
+# i回飲む -> N-iの空いていない, iの空いている
+
+# 行動選択
+# 最大値=動的計画法
+
+# seal 最大, seal = d[x][y]
+# iが同じ動作はできる, 動的計画法しづらい
+
+notEmpty = N
+empty = 0
+
+A = [] # 渡す瓶
+B = [] # 貰う瓶(シールは毎回1枚)
+
+# 優先行動, A/Bが一番いい行動をとるようにする
+# A/Bが悪い、かつ消費が多いものは排他
+# 
+
+for m in M:
+    a, b = map(int,input().split())
+    A.append(a)
+    B.append(b)
+
+while True:
