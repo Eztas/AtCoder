4f0b40f485b067eb62c835e301d512753b323b36
Eztas <nietzsche2669sensei@gmail.com>
2025-07-19 12:06:23 +0000

    solve: abc415, a

--- contest/abc415/a/main.py
+++ contest/abc415/a/main.py
@@ -1,4 +1,14 @@
-S = input()
-L = int(input())
-D = list(map(int,input().split()))
-N, Q = map(int,input().split())
+N = int(input())
+A = list(map(int,input().split()))
+X = int(input())
+
+flag = False
+for a in A:
+    if X == a:
+        flag = True
+
+if flag:
+    print("Yes")
+else:
+    print("No")
+    
