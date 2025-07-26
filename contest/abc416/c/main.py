d36640c8360658b13c771e89f03227a5800601ef
Eztas <nietzsche2669sensei@gmail.com>
2025-07-26 12:34:36 +0000

    wip: abc416, c, 考え方のみ追加, indexで考える

--- contest/abc416/c/main.py
+++ contest/abc416/c/main.py
@@ -1,4 +1,20 @@
-S = input()
-L = int(input())
-D = list(map(int,input().split()))
-N, Q = map(int,input().split())
+N, K, X = map(int,input().split())
+
+# N^K = 10^5
+# S=10, S*N^K = 10^6
+# ソート
+# index配列を作る
+# 挿入ソートが使えそう
+# ソートしていると
+# N=3, K=2, X=6なら
+# 1,1, 1,2, 1,3, の順で辞書順になるため
+# countを増やしてその時の番号をつけて、
+# indexの文字を表示させたらいい
+
+index_S = [0] * N
+S = []
+for n in range(N):
+    s = input()
+    S.append(s)
+
+print(index_S)
