724d1a9c83da7b4deb34c2786ca0002de66ef2c9
Eztas <nietzsche2669sensei@gmail.com>
2025-07-26 15:24:08 +0000

    add: abc416, c, AC13, WA13, RE1, Xを1スタートから0スタートに変更するも完全にはうまくいかず

--- contest/abc416/c/main.py
+++ contest/abc416/c/main.py
@@ -1,17 +1,6 @@
 import numpy as np
 N, K, X = map(int,input().split())
 
-# N^K = 10^5
-# S=10, S*N^K = 10^6
-# ソート
-# index配列を作る
-# 挿入ソートが使えそう
-# ソートしていると
-# N=3, K=2, X=6なら
-# 1,1, 1,2, 1,3, の順で辞書順になるため
-# countを増やしてその時の番号をつけて、
-# indexの文字を表示させたらいい
-
 S = []
 for n in range(N):
     s = input()
@@ -24,10 +13,11 @@ sorted_S = sorted(S)
 # 2進数みたいにすればいける
 
 A = [0] * K
+X = X - 1
 
 str_X = list(str(np.base_repr(X, N)).zfill(K))
 for k in range(K):
-    A[k] = int(str_X[K-1-k])
+    A[k] = int(str_X[k])
 
 for a in A:
     print(sorted_S[a], end='')
