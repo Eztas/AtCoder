a2a287b6f2d3212e01149ea970d806bdd6f34c6d
Eztas <nietzsche2669sensei@gmail.com>
2025-07-26 13:00:17 +0000

    solve: abc416, b, 変化パターン総当たり

--- contest/abc416/b/main.py
+++ contest/abc416/b/main.py
@@ -1,16 +1,31 @@
 S = input()
-T = ''
+#T = S
+T = []
+for s in S:
+    T.append(s)
 
-# S_i= # であるとき、またそのときに限り T_i= #
-# T_i=T_j = o (i<j) ならば、 
-# T_i+1,…,T_j−1の中に # が 1 つ以上存在する。
-# oo連続はありえない
-# #は1つ以上
-# 最初から見て、#があれば、その時の可能な範囲で最先端と最後端を
+def print_horizontal_line(dataList, endChar):
+    for idx, data in enumerate(dataList):
+        if idx < len(dataList) - 1:
+            print(data,end=endChar)
+        else:
+            print(data)
 
-for s in range(len(S)):
-    if S[s] == '#':
-        T += '#'
-    else:
-        T += '.'
+dot_pos = -1
+sharp_flag = False
+for i in range(len(S)):
+    if S[i] == '.' and dot_pos == -1:
+        dot_pos = i
+        T[i] = 'o'
+
+    elif S[i] == '#' and dot_pos != -1 and sharp_flag == False:
+        sharp_flag = True
+
+    elif S[i] == '.' and dot_pos != -1 and sharp_flag == True:
+        T[dot_pos] = 'o'
+        T[i] = 'o'
+        dot_pos = i
+        sharp_flag = False
+
+print_horizontal_line(T, '')
         
