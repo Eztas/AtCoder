d0024f27181629c631313c33afadc8bab065cbcd
Eztas <nietzsche2669sensei@gmail.com>
2025-08-03 13:25:13 +0000

    tle: abc417, c, 計算量考慮せずにいってみる, サンプルは全てクリア

--- contest/abc417/c/main.py
+++ contest/abc417/c/main.py
@@ -1,4 +1,13 @@
-S = input()
-L = int(input())
-D = list(map(int,input().split()))
-N, Q = map(int,input().split())
+N = int(input())
+A = list(map(int,input().split()))
+
+# j - i = A_i + A_j
+
+count = 0
+
+for j in range(1, N):
+    for i in range(j):
+        if j - i == A[i] + A[j]:
+            count += 1
+
+print(count)
