2b5576df90b388813c21275d85ee4d66e9242436
Eztas <nietzsche2669sensei@gmail.com>
2025-08-24 12:44:53 +0000

    solve: abc420, c, 毎度合計からmin(a, b)引かれて足すのを繰り返すだけ

--- contest/abc420/c/main.py
+++ contest/abc420/c/main.py
@@ -2,19 +2,26 @@ N, Q = map(int,input().split())
 A = list(map(int,input().split()))
 B = list(map(int,input().split()))
 
+total = 0
+for n in range(N):
+    total += min(A[n], B[n])
+
 for q in range(Q):
-    total = 0
     query = list(input().split())
     c = query[0]
     X = int(query[1])
     V = int(query[2])
+
+    total -= min(A[X - 1], B[X - 1])
+
+    a = A[X - 1]
+    b = B[X - 1]
 
     if c == 'A':
         A[X - 1] = V
     else:
         B[X - 1] = V
 
-    for n in range(N):
-        total += min(A[n], B[n])
+    total += min(A[X - 1], B[X - 1])
 
     print(total)
