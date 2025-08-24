2c68880410b63ac18ae934b30a1f64775fbc7596
Eztas <nietzsche2669sensei@gmail.com>
2025-08-24 12:38:09 +0000

    add: abc420, c, サンプルクリア, 順当に行ったがおそらく計算超過する

--- contest/abc420/c/main.py
+++ contest/abc420/c/main.py
@@ -1,4 +1,20 @@
-S = input()
-L = int(input())
-D = list(map(int,input().split()))
 N, Q = map(int,input().split())
+A = list(map(int,input().split()))
+B = list(map(int,input().split()))
+
+for q in range(Q):
+    total = 0
+    query = list(input().split())
+    c = query[0]
+    X = int(query[1])
+    V = int(query[2])
+
+    if c == 'A':
+        A[X - 1] = V
+    else:
+        B[X - 1] = V
+
+    for n in range(N):
+        total += min(A[n], B[n])
+
+    print(total)
