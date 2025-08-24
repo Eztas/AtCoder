56c20c66fa6f9b4511b0372c8df6305deac475fb
Eztas <nietzsche2669sensei@gmail.com>
2025-08-24 12:49:49 +0000

    wip: abc420, d, 移動可能なマスだけ列挙

--- contest/abc420/d/main.py
+++ contest/abc420/d/main.py
@@ -1,4 +1,9 @@
-S = input()
-L = int(input())
-D = list(map(int,input().split()))
-N, Q = map(int,input().split())
+H, W = map(int,input().split())
+
+A = []
+for _ in W:
+    s = input()
+    A.append(list(s))
+
+# 高橋君は、 1回の操作で今いるマスから上下左右に隣り合う、障害物マスでも閉じたドアでもないマスへ移動することができます。
+# = ., o, ?, S, Gの5つを移動可能
