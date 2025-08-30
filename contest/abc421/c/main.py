010e55bbc5e50207d284527c3f8fd611cd157247
Eztas <nietzsche2669sensei@gmail.com>
2025-08-30 12:40:42 +0000

    wip: abc421, c, 2種類の考え方に関する途中経過追加

--- contest/abc421/c/main.py
+++ contest/abc421/c/main.py
@@ -1,4 +1,22 @@
+N = int(input())
 S = input()
-L = int(input())
-D = list(map(int,input().split()))
-N, Q = map(int,input().split())
+
+# 実際に移動させる必要はない
+
+# 方法1
+# A始動ならBの移動回数を記せば良い
+# S="AAABABBB", ならS[3] = Bの時、S[1]まで移動させる回数だけ書く
+# 結局2N回検索が必要, 別にN^2じゃないから大丈夫だ
+# 方法2, リスト半分(奇数番目, 偶数番目)
+# 奇数番目は全部同じにしたい, 偶数ばんめも同じ
+
+another_letter_index = 1
+count = 0
+top_s = S[0]
+odd_S = S[0::2]
+even_S = S[1::2]
+
+for i in range(1, 2*N):
+    if S[i] == S[0]:
+        continue
+    else: # S[i] != S[0]
