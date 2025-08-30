2b33aaaaa1e0d8c7cdcf918346e8a17b3c9f48b3
Eztas <nietzsche2669sensei@gmail.com>
2025-08-30 12:12:26 +0000

    solve: abc421, a

--- contest/abc421/a/main.py
+++ contest/abc421/a/main.py
@@ -1,4 +1,17 @@
-S = input()
-L = int(input())
-D = list(map(int,input().split()))
-N, Q = map(int,input().split())
+N = int(input())
+
+S = []
+
+for n in range(N):
+    s = input()
+    S.append(s)
+
+XY = list(input().split())
+
+X = int(XY[0]) - 1
+Y = XY[1]
+
+if S[X] == Y:
+    print("Yes")
+else:
+    print("No")
