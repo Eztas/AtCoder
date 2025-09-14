d7a7ed16236a14eef8b80f7187d709cea44f94c3
Eztas <nietzsche2669sensei@gmail.com>
2025-09-14 12:55:54 +0000

    wip: abc423, d, いけそうな手法が計算量オーバーになりそう

--- contest/abc423/d/main.py
+++ contest/abc423/d/main.py
@@ -1,4 +1,24 @@
 S = input()
-L = int(input())
-D = list(map(int,input().split()))
-N, Q = map(int,input().split())
+N = int(input())
+K = int(input())
+
+k = 0
+# 最大0~Aまでの長さのリストがそれぞれ人数を格納していれば良い
+# 格納する時にN*latestTimeの時間量になる
+A = []
+B = []
+C = []
+latestTime = 0
+for n in range(N):
+    a, b, c = map(int,input().split())
+    A.append(a)
+    B.append(b)
+    C.append(c)
+
+    if a+b > latestTime:
+        latestTime = a+b
+
+L = [0] * (latestTime) # 最悪 A+max(B)
+
+for n in range(N):
+
