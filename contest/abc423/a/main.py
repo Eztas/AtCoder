ab6e6141246e9a16e21783e766a7a5aa159417be
Eztas <nietzsche2669sensei@gmail.com>
2025-09-14 12:08:09 +0000

    solve: abc423, a, 1000円単位にする計算が地味に面倒

--- contest/abc423/a/main.py
+++ contest/abc423/a/main.py
@@ -1,4 +1,11 @@
-S = input()
-L = int(input())
-D = list(map(int,input().split()))
-N, Q = map(int,input().split())
+import math
+X, C = map(int,input().split())
+
+# X = Y + Y * C/1000
+# 1000X = 1000Y + CY
+# Y = 1000X / (1000 + C)
+# Yは1000円単位
+
+real_money = math.floor(1000 * X / (1000 + C) / 1000) * 1000
+
+print(real_money)
