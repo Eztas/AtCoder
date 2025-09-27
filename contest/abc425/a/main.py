3c2ae14be9b52eaf89aee09d24b469ff3ae9e2a4
Eztas <nietzsche2669sensei@gmail.com>
2025-09-27 12:03:46 +0000

    solve: abc425

--- contest/abc425/a/main.py
+++ contest/abc425/a/main.py
@@ -1,4 +1,7 @@
-S = input()
-L = int(input())
-D = list(map(int,input().split()))
-N, Q = map(int,input().split())
+N = int(input())
+
+count = 0
+for n in range(1, N+1):
+    count += ((-1) ** n) * (n ** 3)
+
+print(count)
