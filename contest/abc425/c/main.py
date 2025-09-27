cada6781a4fa48179959190c0e51079e7b3c4ea5
Eztas <nietzsche2669sensei@gmail.com>
2025-09-27 12:56:09 +0000

    wip: abc425, c, 途中経過, 単純計算ver(ただこれをやると計算超過)

--- contest/abc425/c/main.py
+++ contest/abc425/c/main.py
@@ -1,4 +1,19 @@
-S = input()
-L = int(input())
-D = list(map(int,input().split()))
 N, Q = map(int,input().split())
+
+# 移動しかしないから
+# 足し算のパターンは限られている
+
+# 実際に移動させる必要はない
+A = list(map(int,input().split()))
+head = 0
+for q in range(Q):
+    query = list(map(int,input().split()))
+
+    if query[0] == 1:
+        c = query[1]
+        head = (head + c) % N
+
+    else: # 2の時
+        l = query[1]
+        r = query[2]
+        print(sum(A[l-1:r]))
