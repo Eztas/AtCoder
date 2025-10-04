3dbd242c80ee62696743fa2c0cc63ae43049154a
Eztas <nietzsche2669sensei@gmail.com>
2025-10-04 12:17:30 +0000

    add: abc426, c, 計算量無視でサンプルクリア

--- contest/abc426/c/main.py
+++ contest/abc426/c/main.py
@@ -1,4 +1,22 @@
-S = input()
-L = int(input())
-D = list(map(int,input().split()))
 N, Q = map(int,input().split())
+
+# N台のPC
+# 最初はバージョン1~NのN種類のバージョンのPC
+# Xを含むX以前のPCのバージョンをYにする
+# あくまでアップグレードだから、Yが今のものよりも古ければ何もしない
+
+minVer = 1
+
+PC = [n+1 for n in range(N)]
+
+for q in range(Q):
+    X, Y = map(int,input().split())
+    count = 0
+
+    for i in range(X):
+        if PC[i] <= X:
+            PC[i] = Y
+            count += 1
+
+    print(count)
+    
