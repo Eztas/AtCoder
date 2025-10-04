52209c40ccc4953befe90d906987c89c702c78d0
Eztas <nietzsche2669sensei@gmail.com>
2025-10-04 12:08:10 +0000

    solve: abc426, a, ゴリ押し

--- contest/abc426/a/main.py
+++ contest/abc426/a/main.py
@@ -1,4 +1,14 @@
-S = input()
-L = int(input())
-D = list(map(int,input().split()))
-N, Q = map(int,input().split())
+X, Y = input().split()
+
+if Y == "Ocelot":
+    print("Yes")
+elif Y == "Serval":
+    if X == "Ocelot":
+        print("No")
+    else:
+        print("Yes")
+else:
+    if X == "Lynx":
+        print("Yes")
+    else:
+        print("No")
