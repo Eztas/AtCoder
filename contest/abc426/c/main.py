39099767a7bd35932cb91c77a51339e0f96e8853
Eztas <nietzsche2669sensei@gmail.com>
2025-10-04 12:38:47 +0000

    wip: abc426, c, 計算を省く式を入れるも全て失敗

--- contest/abc426/c/main.py
+++ contest/abc426/c/main.py
@@ -4,7 +4,9 @@ N, Q = map(int,input().split())
 # 最初はバージョン1~NのN種類のバージョンのPC
 # Xを含むX以前のPCのバージョンをYにする
 # あくまでアップグレードだから、Yが今のものよりも古ければ何もしない
-
+# 12345
+# 44345
+# やはり代入が良くないが、規則性もなく代入以外の管理がない
 minVer = 1
 
 PC = [n+1 for n in range(N)]
@@ -13,10 +15,15 @@ for q in range(Q):
     X, Y = map(int,input().split())
     count = 0
 
-    for i in range(X):
-        if PC[i] <= X:
-            PC[i] = Y
-            count += 1
+    if minVer <= X:
+        for i in range(X):
+            if PC[i] <= X:
+                PC[i] = Y
+                count += 1
+            else:
+                break
+        minVer = X + 1
+        PC.sort()
 
     print(count)
     
