26c6d8cb57a3586185770e92577cbb9ca32a5abf
Eztas <nietzsche2669sensei@gmail.com>
2025-11-05 11:44:21 +0000

    solve: abc430, b, 重複する時はsetを使う

--- contest/abc430/b/main.py
+++ contest/abc430/b/main.py
@@ -1,4 +1,14 @@
-S = input()
-L = int(input())
-D = list(map(int,input().split()))
-N, Q = map(int,input().split())
+N, M = map(int,input().split())
+S = []
+for n in range(N):
+    s = input()
+    S.append(s)
+
+# 解説を見たらsetを使うらしい(重複の処理に困ったらsetを使う認識でいこう)
+
+G = set()
+for i in range(N-M+1):
+    for j in range(N-M+1):
+        G.add(tuple(S[ii][j:j+M] for ii in range(i, i+M)))
+
+print(len(G))
