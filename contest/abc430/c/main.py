4c0ad53d024d9a50313ce4d81afe6b207c2c01be
Eztas <nietzsche2669sensei@gmail.com>
2025-11-05 13:24:30 +0000

    wip: abc430, c, 累積和の方向性

--- contest/abc430/c/main.py
+++ contest/abc430/c/main.py
@@ -1,4 +1,17 @@
+N, A, B = map(int,input().split())
 S = input()
-L = int(input())
-D = list(map(int,input().split()))
-N, Q = map(int,input().split())
+
+count = 0
+a_count = 0
+b_count = 0
+ab_counts = []
+if len(set(S)) == 1:
+    if S[0] == 'b':
+        print(0)
+
+for n in range(N):
+    if S[n] == 'a':
+        a_count += 1
+    else:
+        b_count += 1
+    ab_counts.append([a_count, b_count])
