018fbdd97bac6d3940433be2da32d6aa4c3523f8
Eztas <nietzsche2669sensei@gmail.com>
2025-11-16 01:47:10 +0000

    refactor: abc432, a, 数字を文字列のまま扱う

--- contest/abc432/a/main.py
+++ contest/abc432/a/main.py
@@ -1,11 +1,3 @@
-A, B, C = map(int,input().split())
-
-num = []
-num.append(100*A+10*B+C)
-num.append(100*A+10*C+B)
-num.append(100*B+10*A+C)
-num.append(100*B+10*C+A)
-num.append(100*C+10*A+B)
-num.append(100*C+10*B+A)
-
-print(max(num))
+A, B, C = input().split()
+num = sorted([A, B, C])
+print(num[2] + num[1] + num[0])
