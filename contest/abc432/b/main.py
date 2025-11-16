be81fcb987ffc2eeb9ee73d413a9cbe62904256c
Eztas <nietzsche2669sensei@gmail.com>
2025-11-16 01:51:55 +0000

    refactor: abc432, b, flagなしでもクリア

--- contest/abc432/b/main.py
+++ contest/abc432/b/main.py
@@ -1,17 +1,6 @@
-X = int(input())
-str_X = str(X)
-
-sorted_X = sorted(str_X, reverse=False)
-zero_flag = False
-for idx, x in enumerate(sorted_X):
-    if zero_flag == False and x != '0':
-        print(''.join(sorted_X))
+X = sorted(input()) # 最初からソート
+for i in range(len(X)):
+    if X[i] != '0':
+        X[0], X[i] = X[i], X[0]
         break
-    if zero_flag == True and x != '0':
-        sorted_X[0] = x
-        sorted_X[idx] = '0'
-        print(''.join(sorted_X))
-        break
-    if x == '0':
-        zero_flag = True
-        
+print(''.join(X))
