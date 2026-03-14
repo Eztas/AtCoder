5d2f10cb6c31a2a3e4b7e8a2c647943fb84b2e8d
Eztas <nietzsche2669sensei@gmail.com>
2026-03-14 12:37:49 +0000

    wip: abc449, c, 累積和の模索

--- contest/abc449/c/main.py
+++ contest/abc449/c/main.py
@@ -1,3 +1,4 @@
+import itertools
 N, L, R = map(int,input().split())
 S = input()
 
@@ -8,13 +9,19 @@ S = input()
 # Lの差、L+1の差、、、Rの差の分で同じものがあるかを確認
 
 # a, b, cに対応した配列
-alphas = [chr(i+97) for i in range(26)]
 alphas_idx = [[] for _ in range(26)]
+alphas_accum_idx = []
 # 0なら更新
 # 1以上なら、差分行列のみを保存
 
 for idx, s in enumerate(S):
     alphas_idx[ord(s) - 97].append(idx)
 
+for idx in alphas_idx:
+    alphas_accum_idx.append(list(itertools.accumulate(idx)))
+
 for id in alphas_idx:
+    print(id)
+
+for id in alphas_accum_idx:
     print(id)
