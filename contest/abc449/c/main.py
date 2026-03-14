9ca8d6dda214e96c88e077034aeb3b6e772fe343
Eztas <nietzsche2669sensei@gmail.com>
2026-03-14 12:30:02 +0000

    feat: abc449, c, ゴリ押しだけどインデックスだけ計算

--- contest/abc449/c/main.py
+++ contest/abc449/c/main.py
@@ -5,3 +5,16 @@ S = input()
 # S_i = S_j
 # L <= j - i <= R
 # 別にabcの文字限定ではなく、26文字の小文字アルファベットが対象
+# Lの差、L+1の差、、、Rの差の分で同じものがあるかを確認
+
+# a, b, cに対応した配列
+alphas = [chr(i+97) for i in range(26)]
+alphas_idx = [[] for _ in range(26)]
+# 0なら更新
+# 1以上なら、差分行列のみを保存
+
+for idx, s in enumerate(S):
+    alphas_idx[ord(s) - 97].append(idx)
+
+for id in alphas_idx:
+    print(id)
