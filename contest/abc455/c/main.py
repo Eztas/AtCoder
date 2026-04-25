68cfb655c731e88984e9499e7ebd8b19a252bb71
Eztas <91040641+Eztas@users.noreply.github.com>
2026-04-25 22:49:38 +0900

    add: abc455, c, heapqとsetを使ってみた

--- contest/abc455/c/main.py
+++ contest/abc455/c/main.py
@@ -1,4 +1,5 @@
 import collections
+import heapq
 N, K = map(int,input().split())
 A = list(map(int,input().split()))
 
@@ -13,16 +14,22 @@ A = list(map(int,input().split()))
 # print(c.items())
 # dict_items([('a', 4), ('b', 1), ('c', 2)])
 
-C_A = collections.Counter(A)
-total = sum(A)
-k_list = [0] * K
-for c_a in C_A.keys():
-  for k in K:
-    if c_a * C_A[c_a] >= k_list[k]:
-      k_list[k] = c_a * C_A[c_a]
-      break
+set_a = set(A)
+total_A = sum(A)
+min_count = len(set_a) - K
+max_data = 0
+k_list = [0]*K
+heapq.heapify(k_list) # 返り値を返す関数ではない
 
-for k in k_list:
-  total -= k
-  
-print(total)
+if min_count <= 0:
+  print(0)
+
+else:
+  for solo_a in set_a:
+    pop_k = heapq.heappop(k_list)
+    if pop_k < solo_a * A.count(solo_a):
+      heapq.heappush(k_list, solo_a * A.count(solo_a))
+    else:
+      heapq.heappush(k_list, pop_k)
+      
+  print(total_A - sum(k_list))
