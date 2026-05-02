024d76493aff29ff4b54dc4e793e8b823a12d555
Eztas <nietzsche2669sensei@gmail.com>
2026-05-02 12:03:05 +0000

    solve: abc456, a

--- contest/abc456/a/main.py
+++ contest/abc456/a/main.py
@@ -1,12 +1,7 @@
-import collections
-import heapq
-# set(L), L.count(l), heapq.heapify(L)
+X = int(input())
 
-s = input()
-s_list = list(input()) # 入力例: "abcde" -> ['a', 'b', 'c', 'd', 'e']
-S = input().split() # 入力例: ". # ." -> ['.', '#', '.']
-T = [list(input()) for _ in range(H)]
-
-M = int(input())
-A = list(map(int,input().split())) # 配列で返す
-N, Q = map(int,input().split()) # それぞれの変数に数値を渡す
+if X < 3 or X > 18:
+    print("No")
+else:
+    print("Yes")
+    
