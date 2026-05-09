8a0ad1497a880ac4f4a21391053a7fc49da9df56
Eztas <nietzsche2669sensei@gmail.com>
2026-05-09 12:02:32 +0000

    solve: abc457

--- contest/abc457/a/main.py
+++ contest/abc457/a/main.py
@@ -1,12 +1,5 @@
-import collections
-import heapq
-# set(L), L.count(l), heapq.heapify(L)
-
-s = input()
-s_list = list(input()) # 入力例: "abcde" -> ['a', 'b', 'c', 'd', 'e']
-S = input().split() # 入力例: ". # ." -> ['.', '#', '.']
-T = [list(input()) for _ in range(H)]
-
-M = int(input())
+N = int(input())
 A = list(map(int,input().split())) # 配列で返す
-N, Q = map(int,input().split()) # それぞれの変数に数値を渡す
+X = int(input())
+
+print(A[X-1])
