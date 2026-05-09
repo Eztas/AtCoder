93ce99ac145ad7c5639408378c350d8744ec70ef
Eztas <nietzsche2669sensei@gmail.com>
2026-05-09 12:56:40 +0000

    wip: abc457, e, 問題を見るだけ見る

--- contest/abc457/e/main.py
+++ contest/abc457/e/main.py
@@ -2,11 +2,22 @@ import collections
 import heapq
 # set(L), L.count(l), heapq.heapify(L)
 
-s = input()
-s_list = list(input()) # 入力例: "abcde" -> ['a', 'b', 'c', 'd', 'e']
-S = input().split() # 入力例: ". # ." -> ['.', '#', '.']
-T = [list(input()) for _ in range(H)]
+N, M = map(int,input().split()) # それぞれの変数に数値を渡す
 
-M = int(input())
-A = list(map(int,input().split())) # 配列で返す
-N, Q = map(int,input().split()) # それぞれの変数に数値を渡す
+L = []
+R = []
+for m in range(M):
+    l, r = map(int,input().split())
+    L.append(l)
+    R.append(r)
+
+# 各2枚の組み合わせが示す布パターンをやるか？
+# 
+
+Q = int(input())
+
+for q in range(Q):
+    S, T = map(int,input().split())
+
+    # S, Tの範囲だけが覆われる, Mの中の2枚を選ぶことができるか
+    # l, rを愚直に追うとN*Q通り
