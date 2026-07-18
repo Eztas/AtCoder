109dc12401914535248064124500603345e09dea
Eztas <nietzsche2669sensei@gmail.com>
2026-07-18 12:31:10 +0000

    solve: acb467, b

--- contest/abc467/b/main.py
+++ contest/abc467/b/main.py
@@ -1,14 +1,18 @@
-import collections
-import heapq
-from bisect import bisect_right, bisect_left # x以下の最小値のidx, x以上の最大値のidxを返す
-from collections import defaultdict # setっぽく使える
-# set(L), L.count(l), heapq.heapify(L)
+N = int(input())
+A = []
+B = []
+S = []
 
-s = input()
-s_list = list(input()) # 入力例: "abcde" -> ['a', 'b', 'c', 'd', 'e']
-S = input().split() # 入力例: ". # ." -> ['.', '#', '.']
-T = [list(input()) for _ in range(H)]
+for n in range(N):
+    a, b, s = input().split()
 
-M = int(input())
-A = list(map(int,input().split())) # 配列で返す
-N, Q = map(int,input().split()) # それぞれの変数に数値を渡す
+    A.append(int(a))
+    B.append(int(b))
+    S.append(s)
+
+loss = 0
+for i in range(N):
+    if S[i] == "keep":
+        loss += B[i] - A[i]
+
+print(loss)
