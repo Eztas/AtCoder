e90d9e2eed855d334d0ac5cb2b5c2674a4da9011
Eztas <nietzsche2669sensei@gmail.com>
2026-07-18 13:11:42 +0000

    add: abc467, a, hの2乗にしたら3WAで割り算にしたら1WA

--- contest/abc467/a/main.py
+++ contest/abc467/a/main.py
@@ -1,14 +1,7 @@
-import collections
-import heapq
-from bisect import bisect_right, bisect_left # x以下の最小値のidx, x以上の最大値のidxを返す
-from collections import defaultdict # setっぽく使える
-# set(L), L.count(l), heapq.heapify(L)
-
-s = input()
-s_list = list(input()) # 入力例: "abcde" -> ['a', 'b', 'c', 'd', 'e']
-S = input().split() # 入力例: ". # ." -> ['.', '#', '.']
-T = [list(input()) for _ in range(H)]
-
-M = int(input())
-A = list(map(int,input().split())) # 配列で返す
-N, Q = map(int,input().split()) # それぞれの変数に数値を渡す
+H, W = map(int,input().split()) # それぞれの変数に数値を渡す
+h = H / 100
+bmi = W / h / h
+if bmi >= 25:
+    print("Yes")
+else:
+    print("No")
