248b78c465d7ef9c4fac5c93afb19d15319834b0
Eztas <nietzsche2669sensei@gmail.com>
2026-07-18 12:49:14 +0000

    wip: abc467, c, テストケース

--- contest/abc467/c/main.py
+++ contest/abc467/c/main.py
@@ -4,11 +4,42 @@ from bisect import bisect_right, bisect_left # x以下の最小値のidx, x以�
 from collections import defaultdict # setっぽく使える
 # set(L), L.count(l), heapq.heapify(L)
 
-s = input()
-s_list = list(input()) # 入力例: "abcde" -> ['a', 'b', 'c', 'd', 'e']
-S = input().split() # 入力例: ". # ." -> ['.', '#', '.']
-T = [list(input()) for _ in range(H)]
-
-M = int(input())
+N, M = map(int,input().split()) # Mは2固定
 A = list(map(int,input().split())) # 配列で返す
-N, Q = map(int,input().split()) # それぞれの変数に数値を渡す
+B = list(map(int,input().split())) # 配列で返す
+
+AA = []
+
+for i in range(N-1):
+    AA.append((A[i]+A[i+1]))
+
+# i番目のA[i]に1を加える、を好きな回数
+# 実質AA[i-1]とAA[i]を1増加
+# A[i]+A[i+1]を2で割った余が、B[i]に等しい
+# 基本0か1
+# 必要な操作回数の最小値
+# 全部でこれを満たすことが重要
+
+# 0 0 0 1 1 0 1 0 1 0 A
+# 0 0 1 2 1 1 1 1 1 AA
+# 0 1 0 1 0 1 0 1 0 B
+# 2つ同士のペアで揃っていないものを消せばいい
+# 偶数ペア数 = /2すればいい？
+
+# 0 0 0 1 1 0 1 0 1 0 A
+# 0 0 1 2 1 1 1 1 1 AA
+# 0 1 1 1 0 1 0 1 0 B
+# 7つのペア
+
+# 0 0 1 1 1 0 1 0 1 0 A
+# 0 1 2 2 1 1 1 1 1 AA
+# 0 1 1 1 0 1 0 1 0 B
+
+# 0 1 3 3 1 1 1 1 1 AA
+# 0 1 1 1 0 1 0 1 0 B
+
+# 0 1 3 3 2 2 1 1 1 AA
+# 0 1 1 1 0 1 0 1 0 B
+
+# 0 1 3 3 2 2 1 1 1 AA
+# 0 1 1 1 0 1 0 1 0 B
