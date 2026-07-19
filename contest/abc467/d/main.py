f9e4847f5e1ef0dba389776e5ad64ae355246857
Eztas <nietzsche2669sensei@gmail.com>
2026-07-19 00:33:01 +0000

    wip: abc467, d, 方程式でやろうとして断念

--- contest/abc467/d/main.py
+++ contest/abc467/d/main.py
@@ -1,11 +1,24 @@
 T = int(input())
 
+# 方程式を使うと遅いから、ベクトルがいいらしい
 for t in range(T):
     PX, PY, QX, QY, RX, RY, SX, SY = map(int,input().split()) # それぞれの変数に数値を渡す
     # C1, C2の中心は同じだが、どこでもいい
     # RとSの直交の直線とPとQの直交直線の式では
     # 
-    anti_C_1_slope = (QY - PY) / (QX - PX)
+    C_1_slope = 0
+    C_2_slope = 0
+
+    if QX - PX == 0 and SX - RX == 0:
+        if 
+        print("No")
+    elif QY - PY == 0 and SY - RY == 0:
+    if QX - PX != 0:
+        anti_C_1_slope = (QY - PY) / (QX - PX)
+        C_1_slope = (-1) / anti_C_1_slope
+    else:
+
+    
     anti_C_2_slope = (SY - RY) / (SX - RX)
     C_1_slope = (-1) / anti_C_1_slope
     C_2_slope = (-1) / anti_C_2_slope
