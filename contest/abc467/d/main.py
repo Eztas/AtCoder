278bb63d35b0ed796d3b1fdecba1cb393c2a8d23
Eztas <nietzsche2669sensei@gmail.com>
2026-07-19 00:44:08 +0000

    add: abc467, d, ベクトルの外積の考え追加

--- contest/abc467/d/main.py
+++ contest/abc467/d/main.py
@@ -1,33 +1,22 @@
 T = int(input())
 
-# 方程式を使うと遅いから、ベクトルがいいらしい
+# 方程式を使うと遅いかつ外部ライブラリが必須だから、ベクトルがいいらしい
+# 空間ベクトル a, b の外積（Cross Product）に関する仕様
+# - 大きさ: |a||b|sinθ （a と b が成す平行四辺形の面積に等しい）
+# - 向き: ベクトル a, b の両方に垂直な方向
+# これさえ求めれば。PQとRSに直交する箇所の交点がわかる
 for t in range(T):
     PX, PY, QX, QY, RX, RY, SX, SY = map(int,input().split()) # それぞれの変数に数値を渡す
     # C1, C2の中心は同じだが、どこでもいい
     # RとSの直交の直線とPとQの直交直線の式では
-    # 
-    C_1_slope = 0
-    C_2_slope = 0
 
-    if QX - PX == 0 and SX - RX == 0:
-        if 
+    vecPQ = [QX - PX, QY - PY]
+    vecRS = [SX - RX, SY - RY]
+    # Y1/X1 = Y2/X2の考えを応用
+
+    # 並行
+    if vecPQ[1] * vecRS[0] - vecPQ[0] * vecRS[1] == 0:
         print("No")
-    elif QY - PY == 0 and SY - RY == 0:
-    if QX - PX != 0:
-        anti_C_1_slope = (QY - PY) / (QX - PX)
-        C_1_slope = (-1) / anti_C_1_slope
-    else:
-
-    
-    anti_C_2_slope = (SY - RY) / (SX - RX)
-    C_1_slope = (-1) / anti_C_1_slope
-    C_2_slope = (-1) / anti_C_2_slope
-
-    # y = cs*X + a
-    C_1_seppen = (QY - PY) / 2 + (C_1_slope*((QX - PX) / 2))
-    C_2_seppen = (SY - RY) / 2 + (C_2_slope*((SX - RX) / 2))
-
-    if C_1_slope == C_2_slope:
-        print("No")
-    else:
+    else: # 並行でない = 交わる = 中心は存在
         print("Yes")
+    
