5f8f5bbf5833c06a2b26a522fe49b88e32641a34
Eztas <nietzsche2669sensei@gmail.com>
2026-08-01 12:21:41 +0000

    solve: abc469, b, Sをlistで受け取る癖を持つ方がいい

--- contest/abc469/b/main.py
+++ contest/abc469/b/main.py
@@ -1,17 +1,15 @@
 N = int(input())
-S = input().split() # 入力例: ". # ." -> ['.', '#', '.']
+S = list(input()) # 入力例: ". # ." -> ['.', '#', '.']
 
 # o=人がいる, x=人いない
 # 椅子がない = 左端
 S_edge = ['x'] + S + ['x']
-print(S_edge)
 count = 0
 
 for n in range(N+2):
     if n == 0 or n == N+1:
         continue
     else:
-        print('n='+str(n))
         if S_edge[n-1] == 'x' and S_edge[n] == 'x' and S_edge[n+1] == 'x':
             count += 1
 
