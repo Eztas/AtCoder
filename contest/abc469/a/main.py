b89885841f4f2cf12b01f026530080760b4a08f4
Eztas <nietzsche2669sensei@gmail.com>
2026-08-01 12:01:46 +0000

    solve: abc469, a

--- contest/abc469/a/main.py
+++ contest/abc469/a/main.py
@@ -1,14 +1,3 @@
-import collections
-import heapq
-from bisect import bisect_right, bisect_left # x以下の最小値のidx, x以上の最大値のidxを返す
-from collections import defaultdict # setっぽく使える
-# set(L), L.count(l), heapq.heapify(L)
+N, K = map(int,input().split()) # それぞれの変数に数値を渡す
 
-s = input()
-s_list = list(input()) # 入力例: "abcde" -> ['a', 'b', 'c', 'd', 'e']
-S = input().split() # 入力例: ". # ." -> ['.', '#', '.']
-T = [list(input()) for _ in range(H)]
-
-M = int(input())
-A = list(map(int,input().split())) # 配列で返す
-N, Q = map(int,input().split()) # それぞれの変数に数値を渡す
+print(N-K+1)
